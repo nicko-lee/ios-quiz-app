d019b0a67a0cc033f5dd38eb32c1e77f4ef3c4c0
Nicko Lee <nicko.lee@anz.com>
2020-03-20 15:42:20 +1100

    Create local JSON file containing question data + parse it
    + Hook up QM to VC so that it is returning the question data to the VC
    so at this point the VC can now tell the View to display this data

--- Quiz/QuizModel.swift
+++ Quiz/QuizModel.swift
@@ -22,10 +22,42 @@ class QuizModel {
     // Note this func doesn't have a return value as we will implement a custom protocol to allow the QuizModel to contact the VC when the questions have finally been returned from over the network
     func getQuestions() {
         
-        // TODO: Go retrieve data
-        
+        // Go retrieve data (could be local path or network call)
+        getLocalJsonFile()
         
         // When it comes back, call the questionsRetrieved method of the delegate
-        delegate?.questionsRetrieved(questions: [Question]())
+        // delegate?.questionsRetrieved(questions: [Question]())
+    }
+    
+    func getLocalJsonFile() {
+        // Get a path to the JSON file in our app bundle
+        let path = Bundle.main.path(forResource: "QuestionData", ofType: ".json") // this returns an optional string hence the need for the guard statement
+        
+        guard path != nil else {
+            // Can read the above as "confirm path is not nil or else handle that case if it is nil:
+            print("Can't find the JSON file")
+            return // jumps out of getLocalJsonFile()
+        }
+        
+        // Create a URL object from that string path
+        let url = URL(fileURLWithPath: path!)
+        
+        // Get the data from that URL & decode the data
+        do{
+            let data = try Data(contentsOf: url) // This is a data object. It represents the data stream or the byte buffer in memory of that resource or file that we are going to access. This initializer potentially throws an error - so we need some error handling (so whenever a method or an init throws u need to use the try keyword and wrap in a do-catch block)
+            
+            // Decode that data into instances of Question struct
+            let decoder = JSONDecoder()
+            let array = try decoder.decode([Question].self, from: data)
+            
+            // Return the question structs to the view controller
+            delegate?.questionsRetrieved(questions: array)
+        } catch {
+            print("Couldn't create data object from file")
+        }
+    }
+    
+    func getRemoteJsonFile() {
+        
     }
 }
