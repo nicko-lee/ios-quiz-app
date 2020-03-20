3ae971f6ea0fb26a891c1fdd1eda59bbe067dfe1
Nicko Lee <nicko.lee@anz.com>
2020-03-20 13:39:12 +1100

    Populate VC, QuizModel and Question struct with some boilerplate code
    - Setup custom protocol to enable communication between VC and QM
      (so when background thread returns with Question data it has a ref
       to the VC so it can notify it and pass the data back to the VC)
    - Test connectivity with print statements

--- Quiz/QuizModel.swift
+++ Quiz/QuizModel.swift
@@ -8,10 +8,24 @@
 
 import Foundation
 
+// A way for the QuizModel to contact the VC
+protocol QuizProtocol {
+    func questionsRetrieved(questions: [Question]) // will pass back an array of questions (i.e. instances of the Questions struct)
+}
+
 class QuizModel {
+    
+    // The other thing we need to do to complete this protocol-delegate pattern. We need to declare a delegate prop inside the QuizModel class
+    var delegate: QuizProtocol? // optional cos at the start nothing is assigned to it
+    
     
     // Note this func doesn't have a return value as we will implement a custom protocol to allow the QuizModel to contact the VC when the questions have finally been returned from over the network
     func getQuestions() {
         
+        // TODO: Go retrieve data
+        
+        
+        // When it comes back, call the questionsRetrieved method of the delegate
+        delegate?.questionsRetrieved(questions: [Question]())
     }
 }
--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -8,20 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, QuizProtocol {
     
     var model = QuizModel()
     var question = [Question]()
     var questionIndex = 0 // keeps track of what question the user is currently viewing and answering
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // Kick off the process to retrive the questions. Notice that nothing gets returned from calling this. We will need to go back to QuizModel to create a custom protocol so we can contact the VC from the QuizModel and notify VC that the questions are ready
+        // How does the QuizModel contact the VC when the questions are ready? When the VC creates an instance of the QM, we will have the VC set itself to the delegate property of the QM class. So that now the QM object has a reference to the VC
+        model.delegate = self // self refers to the VC of course
         model.getQuestions()
     }
 
+    // MARK: QuizProtocol method
+    
+    func questionsRetrieved(questions: [Question]) {
+        print("Hey question are returned!")
+    }
 
 }
 
